8f1b5a6f20e702d412d6145fbf643f7cad534ace
Carlos Eduardo Campos <mailcarloscampos@gmail.com>
2025-01-24 09:55:16 -0300

    "Alterando para 110"

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosEscolhidos = [];
-let numeroLimite = 100;
+let numeroLimite = 110;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 110');
 }
 
 exibirMensagemInicial();
@@ -37,7 +37,7 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    let numeroEscolhido = parseInt(Math.random() * 100 + 1);
+    let numeroEscolhido = parseInt(Math.random() * 110 + 1);
     let quantidadeDeElementosNaLista = listaDeNumerosEscolhidos.length;
 
     if (quantidadeDeElementosNaLista == numeroLimite) {
